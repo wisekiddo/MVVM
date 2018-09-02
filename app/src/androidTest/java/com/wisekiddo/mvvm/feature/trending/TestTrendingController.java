95fcd836471a4ba0e484f6beffcaa641de8ad85d
Ronald Garcia Bernardo <ronald@wisekiddo.com>
2018-09-02 23:08:21 +0800

    merge master

---
+++ app/src/androidTest/java/com/wisekiddo/mvvm/feature/trending/TestTrendingController.java
@@ -0,0 +1,63 @@
+package com.wisekiddo.mvvm.feature.trending;
+
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.wisekiddo.mvvm.R;
+import com.wisekiddo.mvvm.base.TestApplication;
+import com.wisekiddo.mvvm.data.source.remote.TestApiService;
+import com.wisekiddo.mvvm.feature.home.MainActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
+
+
+@RunWith(AndroidJUnit4.class)
+public class TestTrendingController {
+
+    @Inject
+    TestApiService apiService;
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Before
+    public void setUp() {
+        TestApplication.getComponent().inject(this);
+    }
+
+    @Test
+    public void loadRepos() {
+        apiService.setSendError(false);
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.loading_indicator)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.tv_error)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.repo_list)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+        onView(allOf(withId(R.id.tv_repo_name), withText("RxJava"))).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
+    @Test
+    public void loadReposError() {
+        apiService.setSendError(true);
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.loading_indicator)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.repo_list)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+
+        onView(withId(R.id.tv_error)).check(matches(allOf(withText(R.string.api_error_repos), withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE))));
+    }
+}
